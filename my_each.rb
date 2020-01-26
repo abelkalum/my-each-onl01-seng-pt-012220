61887adc6c0b5cea00127935da7cc290b09dd9bb
Abel Kalum <chumbabel@gmail.com>
2020-01-26 07:53:02 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,2 @@
-def my_each(words)
-  if block_given?
-    counter = 0
-    
-    while i < words.length
-      yield(array[i])
-      counter+=1
-    end
-    
-    words
-  else
-    "This block should not run!"
-  end
-end 
-
+def my_each(array)
+  
